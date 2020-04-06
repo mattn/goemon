5c06739ba4838b53a70b0cea8c270905cb48eba3
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-04-06 09:49:49 +0900

    Fix build

--- proc_posix.go
+++ proc_posix.go
@@ -8,14 +8,14 @@ import (
 	"time"
 )
 
-func (g *goemon) spawn() error {
+func (g *Goemon) spawn() error {
 	g.cmd = exec.Command(g.Args[0], g.Args[1:]...)
 	g.cmd.Stdout = os.Stdout
 	g.cmd.Stderr = os.Stderr
 	return g.cmd.Run()
 }
 
-func (g *goemon) terminate(sig os.Signal) error {
+func (g *Goemon) terminate(sig os.Signal) error {
 	if g.cmd != nil && g.cmd.Process != nil {
 		if sig == os.Kill {
 			return g.cmd.Process.Kill()
